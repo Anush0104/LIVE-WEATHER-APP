4818a8fea045fdecaa50055e00e9f114a6ddc3bc
ANUSHKA SINGH <anushkathakur201@gmail.com>
2025-04-20 00:46:34 +0530

    Update front.js

--- front.js
+++ front.js
@@ -1,5 +1,5 @@
 function getWeather() {
-    const apiKey = 'ac95170e1028120386652581686ae45c';
+    const apiKey = 'YOUR API';
     const city = document.getElementById('city').value;
     const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
